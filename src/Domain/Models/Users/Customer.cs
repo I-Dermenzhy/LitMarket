595397d374c697ed5119a49d167b651be050d9c8
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2023-12-06 13:50:40 -0800

    Add IdentityUser classes to Domain

---
+++ src/Domain/Models/Users/Customer.cs
@@ -0,0 +1,11 @@
+﻿using LitMarket.Domain.Models;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Models.Users;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+public class Customer : IdentityUser, IModel<string>
+{
+    public FullAddress FullAddress { get; set; }
+}
---
+++ src/Domain/Models/Users/FullAddress.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models.Users;
+
+public class FullAddress
+{
+    [MaxLength(60)]
+    public string? Country { get; set; }
+
+    [MaxLength(60)]
+    public string? City { get; set; }
+
+    [MaxLength(60)]
+    public string? StreetAddress { get; set; }
+
+    [Length(6, 6)]
+    [RegularExpression(@"^\d{6}$", ErrorMessage = "Postal code must be exactly 6 digits.")]
+    public string? PostalCode { get; set; }
+}
