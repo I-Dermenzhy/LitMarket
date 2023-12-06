a861c962e4c70e7d73e0a503053fd3b5127438b3
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2023-12-06 13:51:15 -0800

    Add Constants project with Roles static class

---
+++ src/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Constants;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Company = "Company";
+    public const string Customer = "Customer";
+    public const string Employee = "Employee";
+}
