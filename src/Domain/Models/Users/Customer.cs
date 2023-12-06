09d3035d250980c56b453a34b3566caaf6be6499
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2023-12-06 14:00:28 -0800

    Remove Company model as I want to merge Company and Customer. They will have all the same properties, except their Roles will be different

--- src/Domain/Models/Users/Customer.cs
+++ src/Domain/Models/Users/Customer.cs
@@ -2,10 +2,16 @@
 
 using Microsoft.AspNetCore.Identity;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Models.Users;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class Customer : IdentityUser, IModel<string>
 {
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
     public FullAddress FullAddress { get; set; }
 }
