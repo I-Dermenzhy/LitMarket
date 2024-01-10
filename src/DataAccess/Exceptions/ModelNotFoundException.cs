a911258a5aee4b9ff5df96bb46857a59cdfbc777
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-11 01:33:56 +0200

    Add custom exception for not found scenario (while using a repository)

---
+++ src/DataAccess/Exceptions/ModelNotFoundException.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Models;
+
+using System.Linq.Expressions;
+
+namespace DataAccess.Exceptions;
+
+public class ModelNotFoundException<T, U>
+    : NotFoundException<T> where T : IModel<U>
+{
+    public ModelNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public ModelNotFoundException(U id)
+        : this($"The {typeof(T)} with the id '{id}' was not found.")
+    {
+    }
+
+    public ModelNotFoundException(Expression<Func<T, bool>> searchFilters)
+        : base(searchFilters)
+    {
+    }
+
+    public ModelNotFoundException(T model)
+        : base(model)
+    {
+        Model = model ?? throw new ArgumentNullException(nameof(model));
+    }
+
+    public T? Model { get; init; }
+}
+
+public class ModelNotFoundException<T>
+    : ModelNotFoundException<T, int> where T : IModel<int>
+{
+    public ModelNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public ModelNotFoundException(int id)
+        : base($"The {typeof(T)} with the id '{id}' was not found.")
+    {
+    }
+
+    public ModelNotFoundException(Expression<Func<T, bool>> searchFilters)
+        : base(searchFilters)
+    {
+    }
+
+    public ModelNotFoundException(T model)
+        : base(model)
+    {
+        Model = model ?? throw new ArgumentNullException(nameof(model));
+    }
+}
---
+++ src/DataAccess/Exceptions/NotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace DataAccess.Exceptions;
+
+public class NotFoundException<T>(string message) : Exception(message)
+{
+    public NotFoundException(Expression<Func<T, bool>> searchFilters)
+        : this($"An entity with the following search filters: {searchFilters} was not found.")
+    {
+        SearchFilters = searchFilters ?? throw new ArgumentNullException(nameof(searchFilters));
+    }
+
+    public NotFoundException(T entity)
+        : this($"The entity {entity} was not found.")
+    {
+        Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+    }
+
+    public T? Entity { get; init; }
+
+    public Expression<Func<T, bool>>? SearchFilters { get; init; }
+}
