d2abaf8ec19339bb024df180220390caa062b52e
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-11 01:33:19 +0200

    Add DbContext for the entire solution

---
+++ src/DataAccess/Data/LibMarketDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Models.Books;
+using Domain.Models.Orders;
+using Domain.Models.Products;
+using Domain.Models.Users;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data;
+
+public class LibMarketDbContext(DbContextOptions<LibMarketDbContext> options)
+    : IdentityDbContext<IdentityUser>(options)
+{
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<BookCategory> BookCategories => Set<BookCategory>();
+    public DbSet<BookImage> BookImages => Set<BookImage>();
+
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    public DbSet<Delivery> Deliveries => Set<Delivery>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<Payment> Payments => Set<Payment>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LibMarketDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+
+        SeedData(modelBuilder);
+    }
+
+    private static void SeedData(ModelBuilder modelBuilder)
+    {
+    }
+}
