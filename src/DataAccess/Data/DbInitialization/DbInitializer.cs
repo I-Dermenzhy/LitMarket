c6ed439b4f93199b6633b07c0590dbac4a1dfab3
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-11 01:35:44 +0200

    Add Db Initializers to apply migration (if any) and initialize the user roles (if not initialized) on the program starting

---
+++ src/DataAccess/Data/DbInitialization/DbInitializer.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data.DbInitialization;
+
+public class DbInitializer(
+    LibMarketDbContext db,
+    UserManager<IdentityUser> userManager,
+    RoleManager<IdentityRole> roleManager)
+    : IDbInitializer
+{
+    private readonly LibMarketDbContext _db = db;
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+    public void Initialize()
+    {
+        ApplyMigrations();
+        AddUserRoles();
+    }
+
+    private void ApplyMigrations()
+    {
+        if (_db.Database.GetPendingMigrations().Any())
+            _db.Database.Migrate();
+    }
+
+    private void AddUserRoles()
+    {
+        if (!_roleManager.Roles.Any())
+        {
+            _roleManager.CreateAsync(new IdentityRole(Roles.Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(Roles.Company)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(Roles.Customer)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(Roles.Employee)).GetAwaiter().GetResult();
+
+            AddAdmin();
+        }
+    }
+
+    private void AddAdmin()
+    {
+        _userManager.CreateAsync(new IdentityUser
+        {
+            UserName = "admin",
+            Email = "admin@gmail.com"
+        }, "Admin123*").GetAwaiter().GetResult();
+
+        IdentityUser admin = _userManager.FindByNameAsync("admin").GetAwaiter().GetResult()!;
+
+        _userManager.AddToRoleAsync(admin, Roles.Admin).GetAwaiter().GetResult();
+    }
+}
