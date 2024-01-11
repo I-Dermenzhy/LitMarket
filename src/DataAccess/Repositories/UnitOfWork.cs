a297b919e45d37dee5618f374c20fafe362f437a
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-11 02:31:17 +0200

    Add UnitOfWork

---
+++ src/DataAccess/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Abstractions.Repositories.Books;
+using Abstractions.Repositories.Orders;
+using Abstractions.Repositories.Users;
+
+using DataAccess.Data;
+
+namespace DataAccess.Repositories;
+
+public sealed class UnitOfWork(
+    LibMarketDbContext db,
+    IBookCategoryRepository bookCategoryRepository,
+    IBookImageRepository bookImageRepository,
+    IBookRepository bookRepository,
+    ICustomerRepository customerRepository,
+    IOrderItemRepository orderItemRepository,
+    IOrderRepository orderRepository,
+    IPaymentRepository paymentRepository,
+    IShippingRepository shippingRepository)
+{
+    private readonly LibMarketDbContext _db = db;
+
+    public IBookCategoryRepository BookCategoryRepository { get; } = bookCategoryRepository;
+    public IBookImageRepository BookImageRepository { get; } = bookImageRepository;
+    public IBookRepository BookRepository { get; } = bookRepository;
+    public ICustomerRepository CustomerRepository { get; } = customerRepository;
+    public IOrderItemRepository OrderItemRepository { get; } = orderItemRepository;
+    public IOrderRepository OrderRepository { get; } = orderRepository;
+    public IPaymentRepository PaymentRepository { get; } = paymentRepository;
+    public IShippingRepository ShippingRepository { get; } = shippingRepository;
+
+    public void Save() => _db.SaveChanges();
+}
