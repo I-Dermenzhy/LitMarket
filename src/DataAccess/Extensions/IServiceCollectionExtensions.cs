d8adb33b87b735a3ba9b44b2d9997fca47ff8efe
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-12 02:55:45 +0200

    Add solution's repositories dependency injection extension

---
+++ src/DataAccess/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Abstractions.Repositories;
+using Abstractions.Repositories.Books;
+using Abstractions.Repositories.Orders;
+using Abstractions.Repositories.Users;
+
+using DataAccess.Repositories;
+using DataAccess.Repositories.Books;
+using DataAccess.Repositories.Orders;
+using DataAccess.Repositories.Users;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddLitMarketRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBookCategoryRepository, BookCategoryRepository>();
+        services.AddScoped<IBookImageRepository, BookImageRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IPaymentRepository, PaymentRepository>();
+        services.AddScoped<IShippingRepository, ShippingRepository>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
