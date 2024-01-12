3a1e55ac942129fd238f97e07509a1393c148131
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-12 02:47:17 +0200

    Add an interface for the UnitOfWork

---
+++ src/Abstractions/Repositories/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Abstractions.Repositories.Books;
+using Abstractions.Repositories.Orders;
+using Abstractions.Repositories.Users;
+
+namespace Abstractions.Repositories;
+
+public interface IUnitOfWork
+{
+    public IBookCategoryRepository BookCategories { get; }
+    public IBookImageRepository BookImages { get; }
+    public IBookRepository Books { get; }
+    public ICustomerRepository Customers { get; }
+    public IOrderItemRepository OrderItems { get; }
+    public IOrderRepository Orders { get; }
+    public IPaymentRepository Payments { get; }
+    public IShippingRepository Shippings { get; }
+
+    public void Save();
+}
