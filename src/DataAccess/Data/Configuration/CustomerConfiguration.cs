7575ac088b6b690bd0a68ad9242cbd2238eb0983
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-12 02:55:08 +0200

    Add configuration to map FullAddress class to related attributes in the 'Shipping' and 'Customer' tables (as there's no 'FullAdress' table)

---
+++ src/DataAccess/Data/Configuration/CustomerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models.Users;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Data.Configuration;
+
+internal sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.OwnsOne(u => u.FullAddress, a =>
+        {
+            a.Property(p => p.Country).HasMaxLength(60);
+            a.Property(p => p.City).HasMaxLength(60);
+            a.Property(p => p.StreetAddress).HasMaxLength(60);
+            a.Property(p => p.PostalCode).HasMaxLength(6)
+                .HasAnnotation("RegularExpression", @"^\d{6}$")
+                .HasAnnotation("ErrorMessage", "Postal code must be exactly 6 digits.");
+        });
+    }
+}
