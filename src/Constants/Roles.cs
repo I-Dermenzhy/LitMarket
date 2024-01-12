a96e98ce8e6f0fd88c4da7ad980e9111d27eddb9
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-12 15:49:22 +0200

    Remove unnecessary project (all constants are now presented as enums in more suitable places)

--- src/Constants/Roles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Constants;
-
-public static class Roles
-{
-    public const string Admin = "Admin";
-    public const string Company = "Company";
-    public const string Customer = "Customer";
-    public const string Employee = "Employee";
-}
