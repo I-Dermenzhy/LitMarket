6be026ee3c3cb9bf195d3f3ab4a82951c0d1c7a6
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-22 23:13:40 +0200

    Specify FullAddress constraints

--- src/Domain/Models/Orders/FullAddress.cs
+++ src/Domain/Models/Orders/FullAddress.cs
@@ -4,16 +4,17 @@ namespace Domain.Models.Users;
 
 public class FullAddress
 {
-    [MaxLength(60)]
-    public string? Country { get; set; }
+	[Length(2, 60)]
+	public string? Country { get; set; }
 
-    [MaxLength(60)]
-    public string? City { get; set; }
+	[Length(2, 60)]
+	public string? City { get; set; }
 
-    [Length(6, 6)]
-    [RegularExpression(@"^\d{6}$", ErrorMessage = "Postal code must be exactly 6 digits.")]
-    public string? PostalCode { get; set; }
+	[Length(6, 6)]
+	[RegularExpression(@"^\d{6}$", ErrorMessage = "Postal code must be exactly 6 digits.")]
+	public string? PostalCode { get; set; }
 
-    [MaxLength(60)]
-    public string? StreetAddress { get; set; }
+	[Length(2, 60)]
+	[MaxLength(60)]
+	public string? StreetAddress { get; set; }
 }
