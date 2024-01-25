7f2f4f39a33105cc32b5c0c983088ec829175b43
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-25 19:23:30 +0200

    Update OrderUpdateRequest structure (instead of 2 Order properties 'Current' and 'RequestedUpdate', olny 'Current' is left with the name of 'Order') to handle problems with storing records in db (as well as the fact that only a limited set of Order properties should be available for modifications)

--- src/Domain/Dto/Orders/OrderUpdateRequestDto.cs
+++ src/Domain/Dto/Orders/OrderUpdateRequestDto.cs
@@ -1,4 +1,4 @@
-﻿using Domain.Dto.Users;
+﻿using Domain.Models.Users;
 
 namespace Domain.Dto.Orders;
 
@@ -6,15 +6,10 @@ namespace Domain.Dto.Orders;
 public class OrderUpdateRequestDto
 {
     public int Id { get; set; }
-    public string CustomerId { get; set; }
-    public CustomerDto Customer { get; set; }
     public int CurrentOrderId { get; set; }
     public OrderDto CurrentOrder { get; set; }
-    public string ShippingId { get; set; }
-    public ShippingDto Shipping { get; set; }
-    public IEnumerable<OrderItemDto> Items { get; set; }
-    public string PaymentId { get; set; }
-    public PaymentDto Payment { get; set; }
-    public double TotalPrice { get; set; }
+    public string? CustomerNameUpdate { get; set; }
+    public FullAddress? ShippingAddressUpdate { get; set; }
+    public string? Reason { get; set; }
     public string Status { get; set; }
 }
--- src/Domain/Models/Orders/OrderUpdateRequest.cs
+++ src/Domain/Models/Orders/OrderUpdateRequest.cs
@@ -1,15 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using Domain.Models.Users;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Models.Orders;
 
 #pragma warning disable CS8618 
-public class OrderUpdateRequest : Order
+public class OrderUpdateRequest : IModel
 {
-    public int CurrentOrderId { get; set; }
+    public int Id { get; set; }
 
-    [ForeignKey(nameof(CurrentOrderId))]
+    public int OrderId { get; set; }
+
+    [ForeignKey(nameof(OrderId))]
     [ValidateNever]
-    public Order CurrentOrder { get; set; }
+    public Order Order { get; set; }
+
+    public string? CustomerNameUpdate { get; set; }
+    public FullAddress? ShippingAddressUpdate { get; set; }
+
+    public string? Reason { get; set; }
+
+    public UpdateRequestStatus Status { get; set; }
+
+    public static OrderUpdateRequest Create(Order current, Order updateRequest,
+        string? reason, UpdateRequestStatus status = UpdateRequestStatus.Pending)
+    {
+        FullAddress currentAddress = current.Shipping.ShippingAddress;
+        FullAddress requestedAddress = updateRequest.Shipping.ShippingAddress;
+
+        FullAddress? shippingAddressUpdate = currentAddress.Equals(requestedAddress)
+            ? null : requestedAddress;
+
+        string? customerNameUpdate = current.Customer.Name.Equals(updateRequest.Customer.Name)
+            ? null : updateRequest.Customer.Name;
+
+        return new OrderUpdateRequest()
+        {
+            OrderId = current.Id,
+            Order = current,
+            CustomerNameUpdate = customerNameUpdate,
+            ShippingAddressUpdate = shippingAddressUpdate,
+            Reason = reason,
+            Status = status
+        };
+    }
+}
+
+public enum UpdateRequestStatus
+{
+    Pending,
+    Approved,
+    Rejected
 }
