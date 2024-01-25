663e39157e31f93170b415ceb2002d21131c17ac
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-25 19:26:49 +0200

    As a part of structural changes of OrderUpdateRequest

---
+++ src/DataAccess/Data/Configuration/OrderUpdateRequestConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.Orders;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Data.Configuration;
+internal sealed class OrderUpdateRequestConfiguration : IEntityTypeConfiguration<OrderUpdateRequest>
+{
+    public void Configure(EntityTypeBuilder<OrderUpdateRequest> builder)
+    {
+        builder.OwnsOne(our => our.ShippingAddressUpdate, a =>
+        {
+            a.Property(p => p.Country).HasMaxLength(60);
+            a.Property(p => p.City).HasMaxLength(60);
+            a.Property(p => p.StreetAddress).HasMaxLength(60);
+            a.Property(p => p.PostalCode).HasMaxLength(6)
+                .HasAnnotation("RegularExpression", @"^\d{6}$")
+                .HasAnnotation("ErrorMessage", "Postal code must be exactly 6 digits.");
+        });
+    }
+}
