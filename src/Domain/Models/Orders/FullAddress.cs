1735771d786a6fe05f430af5c32f077efd26fd61
Ilya Dermenzhy <i.dermenzhy.kp13@kpi.ua>
2024-01-25 19:28:12 +0200

    Add a method to compare FullAddresses to each other based on all properties (would be helpful in the process of creating an OrderUpdateRequest)

--- src/Domain/Models/Orders/FullAddress.cs
+++ src/Domain/Models/Orders/FullAddress.cs
@@ -4,17 +4,37 @@ namespace Domain.Models.Users;
 
 public class FullAddress
 {
-	[Length(2, 60)]
-	public string? Country { get; set; }
+    [Length(2, 60)]
+    public string? Country { get; set; }
 
-	[Length(2, 60)]
-	public string? City { get; set; }
+    [Length(2, 60)]
+    public string? City { get; set; }
 
-	[Length(6, 6)]
-	[RegularExpression(@"^\d{6}$", ErrorMessage = "Postal code must be exactly 6 digits.")]
-	public string? PostalCode { get; set; }
+    [Length(6, 6)]
+    [RegularExpression(@"^\d{6}$", ErrorMessage = "Postal code must be exactly 6 digits.")]
+    public string? PostalCode { get; set; }
 
-	[Length(2, 60)]
-	[MaxLength(60)]
-	public string? StreetAddress { get; set; }
+    [Length(2, 60)]
+    [MaxLength(60)]
+    public string? StreetAddress { get; set; }
+
+    public bool Equals(FullAddress fullAddress)
+    {
+        if (fullAddress is null)
+            return false;
+
+        if (Country != fullAddress.Country)
+            return false;
+
+        if (City != fullAddress.City)
+            return false;
+
+        if (PostalCode != fullAddress.PostalCode)
+            return false;
+
+        if (StreetAddress != fullAddress.StreetAddress)
+            return false;
+
+        return true;
+    }
 }
